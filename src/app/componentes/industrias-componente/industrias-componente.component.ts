93b25e0347aa631799b550ffcce51560b19585a3
AlejandroGarridoV <j.alejandro_garrido@outlook.com>
2025-08-26 18:58:44 -0600

    Se agrega la información de los proyectos y el curriculum

--- src/app/componentes/industrias-componente/industrias-componente.component.ts
+++ src/app/componentes/industrias-componente/industrias-componente.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-industrias-componente',
-  imports: [],
   templateUrl: './industrias-componente.component.html',
-  styleUrl: './industrias-componente.component.css'
+  styleUrls: ['./industrias-componente.component.css']
 })
 export class IndustriasComponenteComponent {
-
+  
 }
